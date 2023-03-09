332a41a70a5caf17a6878905d4eacfd32064116e
unknown <nasiralics99@gmail.com>
2023-03-09 18:00:53 +0500

    adsfa

--- components/HeroSection/index.js
+++ components/HeroSection/index.js
@@ -3,12 +3,12 @@ function index() {
     <div className="lg:h-screen h-[600px]  text-white hero-bg flex flex-col justify-between uppercase lg:p-10 md:p-6 p-4 pb-6">
       <div className="md:grid md:grid-cols-3 flex  flex-col gap-8 items-center w-full ">
         <div className="flex  gap-5 items-end">
-          <img className="md:w-12 w-4" src="./Images/ZIMO Menu Icon W.svg" />
-          <img className="md:w-28 w-12" src="./Images/ZIMO W.png" />
+          <img className="md:w-12 w-4" src="/Images/ZIMO Menu Icon W.svg" />
+          <img className="md:w-28 w-12" src="/Images/ZIMO W.png" />
           <p className="md:text-base text-xs">about</p>
         </div>
         <div>
-          <img className="w-36 flex mx-auto" src="./Images/ZIMO ZIG W.png" />
+          <img className="w-36 flex mx-auto" src="/Images/ZIMO ZIG W.png" />
         </div>
         <div className="flex xl:gap-8 justify-end items-center">
           <div className="flex gap-3 items-center">
@@ -18,15 +18,15 @@ function index() {
             </div>
             <img
               className="md:w-12 w-6 md:h-7 h-5"
-              src="./Images/download.png"
+              src="/Images/download.png"
             />
           </div>
-          <img className="w-14 h-8" src="./Images/ZIMO User Icon W.svg" />
-          <img className="w-8 h-8 invert filter-100" src="./Images/cart.jpg" />
+          <img className="w-14 h-8" src="/Images/ZIMO User Icon W.svg" />
+          <img className="w-8 h-8 invert filter-100" src="/Images/cart.jpg" />
         </div>
       </div>
     <div className="flex gap-2 items-center -mt-10 xl:-mt-24  text-white">
-        <img className="w-8 h-7 invert grayscale-100" src="./Images/arrow.png"/>
+        <img className="w-8 h-7 invert grayscale-100" src="/Images/arrow.png"/>
     <p className="">Back</p> </div>
       <div className="flex flex-col   gap-3">
         <h2 className="text-2xl">Discover</h2>
@@ -35,7 +35,7 @@ function index() {
       </div>
       <div className="flex flex-col gap-8 items-center">
         <h2>bringing the world closer together</h2>
-        <img className="invert grayscale w-20" src="./Images/arrow-down.webp" />
+        <img className="invert grayscale w-20" src="/Images/arrow-down.webp" />
       </div>
     </div>
   );
