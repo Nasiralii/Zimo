c91e4b24197f4bc79917207134de135aa45f40a2
unknown <nasiralics99@gmail.com>
2023-03-10 18:06:14 +0500

    update

--- components/HeroSection/index.js
+++ components/HeroSection/index.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
+
 function index() {
+  const [dropDown,setDropDown]=useState(false);
   return (
     <div className="lg:h-screen h-[600px]  text-white hero-bg flex flex-col justify-between uppercase lg:p-10 md:p-6 p-4 pb-6">
-      <div className="md:grid md:grid-cols-3 flex  flex-col gap-8 items-center w-full ">
-        <div className="flex  gap-5  items-end">
+      <div className="md:grid md:grid-cols-3 flex  md:gap-8 justify-between  items-center w-full ">
+        <div className="flex md:flex-row md:gap-5 gap-2 flex-col  md:items-end">
           <img className="md:w-12 w-4" src="/Images/ZIMO Menu Icon W.svg" />
           <img className="md:w-28 w-12" src="/Images/ZIMO W.png" />
           <p className="md:text-sm text-xs lato-light">about</p>
         </div>
         <div>
-          <img className="w-36 flex mx-auto" src="/Images/ZIMO ZIG W.png" />
+          <img className="md:w-36 w-24 flex mx-auto" src="/Images/ZIMO ZIG W.png" />
         </div>
-        <div className="flex xl:gap-8 justify-end items-center">
+       <div className="md:hidden block">
+       <button className={` ${dropDown===false ? "block " : "hidden"} z-50`} onClick={()=>setDropDown(true)}> 
+        <img className="w-7 h-7 invert grayscale-100 md:hidden block" src="./Images/open.png"/></button>
+        <button className={` ${dropDown===true ? "block " : "hidden"} z-50`} onClick={()=>setDropDown(false)}> 
+        <img className="w-7 h-7 invert grayscale-100 md:hidden blcok" src="./Images/close.png"/>
+        </button>
+       </div>
+        <div className={` md:flex hidden xl:gap-8 justify-end items-center`} >
           <div className="flex gap-3 items-center">
             <div className="flex flex-col md:text-xs lato-thin text-[8px] text-right">
               <p>17:23 london united kingdom</p>
@@ -24,6 +34,25 @@ function index() {
           <img className="w-14 h-8" src="/Images/ZIMO User Icon W.svg" />
           <img className="w-8 h-8 invert filter-100" src="/Images/cart.jpg" />
         </div>
+        {dropDown===true ? 
+        <div className="absolute top-8 z-30  right-0 w-[40%]  ">
+         <div className={`  xl:gap-8 p-2 justify-end items-center my-8 bg-gray-500`} >
+          <div className="flex gap-3 flex-col items-center">
+            <div className="flex flex-col md:text-xs lato-thin text-[8px] text-right">
+              <p>17:23 london united kingdom</p>
+              <p>sunday, 12 february 2023</p>
+            </div>
+            <img
+              className="w-12 my-2 "
+              src="/Images/download.png"
+            />
+          </div>
+         <div className="flex justify-between gap-8">
+         <img className="w-5 " src="/Images/ZIMO User Icon W.svg" />
+          <img className="w-5  invert filter-100" src="/Images/cart.jpg" />
+          </div>
+        </div>
+        </div> :null}
       </div>
     <div className="flex gap-2 items-center -mt-10 xl:-mt-24  text-white">
         <img className="w-8 h-7 invert grayscale-100" src="/Images/arrow.png"/>
--- components/Personlalised/index.js
+++ components/Personlalised/index.js
@@ -4,7 +4,9 @@ function index() {
       <div className="flex flex-col md:gap-0 gap-8 md:p-14 p-4  h-full  md:justify-between">
         <div className="w-full flex justify-end">
           <div className="flex uppercase md:justify-end lg:w-[35%] md:w-[44%]  flex-col md:gap-4 gap-2 text-white ">
-            <h3 className="text-2xl tracking-widest lato-light">personalized for you</h3>
+            <h3 className="text-2xl tracking-widest lato-light">
+              personalized for you
+            </h3>
             <p className="leading-7 opacity-70  tracking-widest lato-light text-xs">
               delivering thousands of personalized alerts everyday.out users can
               be first in line when that opportunity of a lifetime is here
@@ -18,8 +20,11 @@ function index() {
           />
           <div className="flex uppercase items-center gap-2 md:mt-24  tracking-widest lato-light text-xs">
             <p>create user id</p>
-           
-           <img className="rotate-180 flex w-8 h-6 invert grayscale-100" src="./Images/arrow.png"/>
+
+            <img
+              className="rotate-180 flex w-8 h-6 invert grayscale-100"
+              src="./Images/arrow.png"
+            />
           </div>
         </div>
         <div></div>
